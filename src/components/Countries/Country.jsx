36a7f13451bfa445e085e11c76ec1cbb86dd040b
Abrar Zawad <abrarzawad222@gmail.com>
2024-03-16 12:29:51 +0600

    changing

--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import './Country.css'
 const Country = ({country}) => {
-    const {name,flags, population,area} = country;
+    const {name,flags, population,area, cca3} = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisitted = () =>{
+        setVisited(!visited);
+    }
     return (
         <div className='country'>
             <h3>Name: {name.official}</h3>
             <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area:{area} </p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisitted}>Visited</button>
+            {visited && 'I have visited this country'}
         </div>
     );
 };
